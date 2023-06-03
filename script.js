84cf4d84a834c89e6d09586021ed6152862f5c3e
Money <gks184062@gmail.com>
2023-06-03 17:20:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function aboutFunction() {
+    document.documentElement.scrollTop = 640;
+}
+function skillFunction() {
+    document.documentElement.scrollTop = 1230;
+}
+function contactFunction() {
+    document.documentElement.scrollTop = 1800;
+}
+function darkMode() {
+    var element = document.body;
+    // document.body.classList.toggle("dark-mode");
+    element.classList.toggle("dark-mode");
+}
